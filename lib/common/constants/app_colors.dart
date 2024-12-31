99ec62afb20b142a4fb7358338cd17e09b826ced
rogerio-holetz <rogerio.holetz@yahoo.com.br>
2024-12-31 09:52:56 -0300

    Teste de commit

--- lib/common/constants/app_colors.dart
+++ lib/common/constants/app_colors.dart
@@ -7,10 +7,12 @@ class AppColors {
   static const Color white = Color(0xFFFFFFFF);
   static const Color iceWhite = Color(0xFFEEF8F7);
   static const Color grey = Color(0xFF444444);
+
   static const List<Color> greenGradient = [
     Color(0xFF63B5Af),
     Color(0xFF438883),
   ];
+
   static const List<Color> greyGradient = [
     Color(0xFF63B5Af),
     Color(0xFF438883),
